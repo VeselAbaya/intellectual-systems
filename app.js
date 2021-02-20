0a530bba269ceac0275845d7bbba6fabe5371bc0
Antay <vandervise465@gmail.com>
2021-02-20 16:16:24 +0300

    init

---
+++ app.js
@@ -0,0 +1,8 @@
+const Agent = require('./agent') // Импорт агента
+const VERSION = 7 // Версия сервера
+let teamName = "teamA" // Имя команды
+let agent = new Agent(); // Создание экземпляра агента
+require('./socket')(agent, teamName, VERSION) //Настройка сокета
+agent.socketSend("move", `-15 0`) // Размещение игрока на поле
+
+console.log('app stated');
