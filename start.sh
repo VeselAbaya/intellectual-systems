5c723e4aac4fa8f2a4088e7f7da8a17390003f1a
Antay <vandervise465@gmail.com>
2021-04-07 21:39:10 +0300

    add logs dir

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rcssserver > rcssserver.log 2>&1 &
-rcssmonitor > rcssmonitor.log 2>&1 &
+rcssserver > ./log/rcssserver.log 2>&1 &
+rcssmonitor > ./log/rcssmonitor.log 2>&1 &
 export MONITOR=$!
 
 sleep 1 &&
